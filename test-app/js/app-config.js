3f4e780f029a36d9e7cf236b9a4a2c213b0a4cf1
Dmitry Lapynov <lapynow.d@gmail.com>
2017-05-16 15:29:59 +0300

    public/local configuration

---
+++ test-app/js/app-config.js
@@ -0,0 +1,34 @@
+var appConf = {
+    app_id: serverConfig.app_id,
+    app_key: serverConfig.app_key,
+    group: {
+        scope: [
+            'MESSAGES_FROM_GROUP',
+            'GROUP_BOT_API_TOKEN'
+        ].join(';'),
+        scopeMap: {
+            MESSAGES_FROM_GROUP: 'MESSAGES_FROM_GROUP',
+            GROUP_BOT_API_TOKEN: 'GROUP_BOT_API_TOKEN'
+        }
+    },
+    /** application permissions
+     * @see https://apiok.ru/ext/oauth/permissions
+     * */
+    oauth: {
+        scope: [
+            'VALUABLE_ACCESS',
+            'PUBLISH_NOTE',
+            'PUBLISH_TO_STREAM',
+            'GROUP_CONTENT',
+            'PERSONAL_CONTENT',
+            //'APP_INVITE',
+            //'GET_EMAIL',
+            'PHOTO_CONTENT',
+            'SETTINGS',
+            'LIKE'
+        ].join(';'),
+        layout: "w"
+    },
+    widget_server: serverConfig.widget_server,
+    api_server: serverConfig.api_server
+};
---
+++ test-app/js/server-config.js
@@ -0,0 +1,14 @@
+var serverConfig = {
+    appName: 'test-app',
+    domain: 'https://leikam.github.io/ok-applications/test-app', // your external app address
+    widget_server: 'https://connect.ok.ru/',
+    api_server: 'http://apitest.ok.ru',
+    app: {
+        /** application id */
+        app_id: "1250486784",
+        /** application public key */
+        app_key: "CBALOJILEBABABABA"
+    }
+};
+
+
