a8a82bbb461c0dd0491f3f5cad76385969983565
Dmitry Lapynov <lapynow.d@gmail.com>
2017-06-27 19:47:26 +0300

    init back

--- test-app/js/main.js
+++ test-app/js/main.js
@@ -35,7 +35,7 @@ var paramsHandlerMap = {
 
 prepareConfig();
 
-//OKSDK.init(appConf, init_success, init_failure);
+OKSDK.init(appConf, init_success, init_failure);
 
 function init_success() {
     window.console && console.info('Initialization success', appConf);
